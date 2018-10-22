7101de1e380bb58f56387a12525bdbdb3be34270
zhouzhankun <13802885307@139.com>
2018-10-22 09:43:34 +0800

    add task scheduler manager and rxjava

---
+++ common/src/main/java/myframelib/zkzhou/com/common/taskscheduler/callback/Observer.java
@@ -0,0 +1,13 @@
+package myframelib.zkzhou.com.common.taskscheduler.callback;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Observer
+ * Created by D on 2018/1/23.
+ */
+public interface Observer<T> {
+    void onNext(@NonNull T result);
+
+    void onError(Throwable e);
+}
